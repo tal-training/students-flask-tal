700dc71064e222128d236c7fc2b8f1771b35dcf9
Tal Training Repositories <talmanor@live.com>
2023-02-01 00:53:43 +0200

    join

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ def registrations(student_id):
      SELECT students.name FROM students WHERE students.id={student_id} UNION SELECT name from courses WHERE courses.id IN 
           (SELECT course_id FROM students_courses WHERE student_id={student_id})"""
      )
+          # SELECT courses.name, courses.id from courses JOIN students_courses on students_courses.course_id=courses.id where students_courses.student_id=1
      return render_template("registrations.html", courses=course_names)
 
 @app.route('/register/<student_id>/<course_id>')
 def register(student_id, course_id):
      try:
           execute_query(f"INSERT INTO students_courses (student_id, course_id) VALUES ('{student_id}', '{course_id}')")
-          # SELECT courses.name, courses.id from courses JOIN students_courses on students_courses.course_id=courses.id where students_courses.student_id=1
      except IntegrityError:
           return "student is already registered to this course"
      return redirect(url_for('registrations', student_id=student_id))
